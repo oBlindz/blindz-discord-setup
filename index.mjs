15b8da23b06f35aa01086af2c449d59de083c8cd
oBlindz <kakeguruiso2@gmail.com>
2025-03-25 15:28:46 -0300

    created the cjs setup

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 // TOPICS //  
 // [ 1 ] Create the terminal inputs commands : DONE
 // [ 2 ] Instaling the dependencies : DONE
-// [ 3 ] Creating the directories
+// [ 3 ] Creating the directories : DONE 
 //   - commands
 //     - utility (optional)
 // [ 4 ] Creating the files (according the user preference)
@@ -10,14 +10,140 @@
 //   - ping (optional)
 // === //  
 
-// Mkdir SYNC 
-import { mkdirSync } from 'node:fs';
+// mkdirSync & writeFileSync 
+import { mkdirSync, writeFileSync } from 'node:fs';
 
 // Child Process
 import { execSync } from 'node:child_process';
 
 // Global variables
 var testcommand;
+var language;
+
+// cjs function
+function createcjs(tstcmd){
+  // create main.cjs
+  // create deploycommands.cjs
+  // if necessary create testcommand
+  
+  // files code
+  const cjs_main_code = `const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.login(token);
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".cjs"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log('[WARNING] The command at ', filePath, 'is missing a required "data" or "execute" property.');
+    }
+  }
+}
+
+client.once(Events.ClientReady, () => {
+  console.log("Bot online");
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error('No command matching' ,interaction.commandName, ' was found.');
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+    }
+  }
+});
+  `; 
+  const cjs_deploy_code = `const { REST, Routes } = require("discord.js");
+const { clientId, guildId, token } = require("./config.json");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.cjs'));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log('[WARNING] The command at ', filePath, ' is missing a required "data" or "execute" property.');
+    }
+  }
+}
+
+const rest = new REST().setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing ',commands.length,' application (/) commands.');
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+
+    console.log('Successfully reloaded ',data.length, 'application (/) commands.');
+  } catch (err) {
+    console.error(err);
+  }
+})();
+    
+  `; 
+
+  // Test command area
+  if(tstcmd == 'Yes'){
+    const cjs_tst_code = `const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('answer with pong'),
+  async execute(interaction) {
+    await interaction.reply('pong');
+  },
+}
+    `;
+    writeFileSync('./commands/utility/ping.cjs', cjs_tst_code, 'utf8');
+  }
+
+  // Creating the files
+  writeFileSync('main.cjs', cjs_main_code, 'utf8'); 
+  writeFileSync('deploycommands.cjs', cjs_deploy_code, 'utf8');
+}; 
 
 // Temrinal input commands
 import { input, select, confirm } from '@inquirer/prompts';
@@ -56,6 +182,7 @@ function terminalInputs() {
 
     if(projectLanguage) {
       // Call test command
+      language = projectLanguage.valueOf();
       testCommand();
     };
   };
@@ -139,6 +266,35 @@ function terminalInputs() {
           }));
           console.log("'utility' directory created");
         };
+
+        // Start step 4
+          // create .config.json
+          const cfgjson = `{
+"clientId": "",
+"guildId": "",
+"token": ""
+}
+          `;
+
+          writeFileSync('config.json', cfgjson, 'utf8');
+          // Switch between the languages
+          switch (language) {
+            case 'cjs':
+              // Call func cjs
+              createcjs(testcommand);
+              break;
+            case 'mjs': 
+              // Call func mjs
+
+              break;
+            case 'mts': 
+              // Call func mts
+
+              break;
+
+            default:
+              console.log('choose one language');
+          };
     }
   };
 
